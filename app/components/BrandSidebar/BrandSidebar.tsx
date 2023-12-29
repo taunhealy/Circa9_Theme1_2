41d35ff047c7b4b801d7ee675d04d3f44166b253
taunhealy <taunhealy@gmail.com>
2023-12-29 15:17:23 +0200

    fix: brand sidebar arrow scroll indexes 9<

--- app/components/BrandSidebar/BrandSidebar.tsx
+++ app/components/BrandSidebar/BrandSidebar.tsx
@@ -1,11 +1,11 @@
 // BrandSidebar.tsx
-import React, { useRef, useEffect, useState } from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { ChevronUp, ChevronDown } from "lucide-react";
 import "./brandsidebar.css";
 
 interface BrandSidebarProps {
-  brands: (string | null)[]; // Adjust the type to allow null if needed
+  brands: (string | null)[];
   selectedBrand: string | null;
   onBrandClick: (brand: string) => void;
 }
@@ -15,14 +15,15 @@ const BrandSidebar: React.FC<BrandSidebarProps> = ({
   selectedBrand,
   onBrandClick,
 }) => {
-  const containerRef = useRef<HTMLDivElement>(null);
+  const [startIndex, setStartIndex] = useState(0);
 
   const handleScroll = (direction: "up" | "down") => {
-    const container = containerRef.current;
+    const maxStartIndex = Math.max(0, brands.length - 5);
 
-    if (container) {
-      const scrollAmount = direction === "up" ? -30 : 30;
-      container.scrollBy({ top: scrollAmount, behavior: "smooth" });
+    if (direction === "up") {
+      setStartIndex((prevIndex) => Math.max(0, prevIndex - 5));
+    } else {
+      setStartIndex((prevIndex) => Math.min(maxStartIndex, prevIndex + 5));
     }
   };
 
@@ -33,14 +34,14 @@ const BrandSidebar: React.FC<BrandSidebarProps> = ({
       animate={{ opacity: 1, transition: { duration: 2.5 } }}
       exit={{ opacity: 0, transition: { duration: 0.5, delay: 1.2 } }}
     >
-      {brands.length > 3 && (
+      {brands.length > 9 && (
         <motion.div className="arrow-button" onClick={() => handleScroll("up")}>
           <ChevronUp size={20} strokeWidth={2.5} stroke="black" />
         </motion.div>
       )}
 
-      <div className="brands-list" ref={containerRef}>
-        {brands.slice(0, 5).map((brand) => (
+      <div className="brands-list">
+        {brands.slice(startIndex, startIndex + 9).map((brand) => (
           <motion.div
             key={brand || null}
             className={`brand-filter-button ${
@@ -53,7 +54,7 @@ const BrandSidebar: React.FC<BrandSidebarProps> = ({
         ))}
       </div>
 
-      {brands.length > 3 && (
+      {brands.length > 9 && (
         <motion.div
           className="arrow-button"
           onClick={() => handleScroll("down")}
